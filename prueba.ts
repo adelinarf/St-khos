7cc8157215818d55b71895d6e4673e79dcd0fd6f
Adelina Figueira <51302331+adelinarf@users.noreply.github.com>
2022-02-19 22:24:24 -0400

    Add files via upload

--- prueba.ts
+++ prueba.ts
@@ -9,14 +9,16 @@ REPL();
 function lextest(instruccion : String,readline){
 	var token = guardarTokens(instruccion);
 	if (token.length < 1){
-  	console.log("ERROR: Caracter invalido en la entrada "+instruccion);
-  }
-  else{
+		var parseo = parse(instruccion);
+		var pos = parseo.errs[0].pos.overallPos;
+		console.log("ERROR: Caracter invalido ('" + instruccion.charAt(pos) + "') en la entrada");
+	}
+	else{
 		var listaTokens = showTokens(token);
 	  var imprimir = "OK: <" + instruccion + "> ==> " + " [ " + listaTokens + " ] "
 	  var sinEspacios = instruccion.replace(" ","");
 	  console.log(imprimir);
-  }
+	}
 	REPLaux(readline);
 }
 
@@ -29,26 +31,39 @@ function REPL(){
 }
 
 function procesarArchivo(instruccion : String,linea,nombre){
-	var token = guardarTokens(instruccion);
-	var listaTokens = showTokens(token);
-  var imprimir = "OK: <" + instruccion + "> ==> " + " [ " + listaTokens + " ] "
-  console.log(imprimir);
+	var re = /.lex/gi;
+	if (instruccion.search(re) != -1){
+		var instruccionMod = instruccion.replace(re,"");
+		var token = guardarTokens(instruccionMod);
+		if (token.length < 1){
+			var parseo = parse(instruccion);
+			var pos = parseo.errs[0].pos.overallPos;
+			console.log("ERROR: Caracter invalido ('" + instruccion.charAt(pos) + "') en la entrada");
+			errores.push([nombre, linea.toString(),"ERROR: Caracter invalido en la entrada"]);
+		}
+		else{
+			var listaTokens = showTokens(token);
+			var imprimir = "OK: <" + instruccion + "> ==> " + " [ " + listaTokens + " ] "
+			var sinEspacios = instruccion.replace(" ","");
+			console.log(imprimir);
+		}
+	}
+	else{
+		console.log("ERROR: Interpretacion no implementada");
+		errores.push([nombre, linea.toString(),"ERROR: Interpretacion no implementada"]);
+	}
 }
 
 function leerArchivo(name,readline){
 	const fs = require('fs');
 
 	try {
-	    // read contents of the file
 	    const data = fs.readFileSync(name, 'UTF-8');
 
-	    // split the contents by new line
 	    const lines = data.split(/\r?\n/);
-	    console.log(lines);
-	    // print all lines
 	    var nLinea = 0;
 	    for (var i = 0; i < lines.length; i++) {
-	      procesarArchivo(lines[i],nLinea,name);
+	      procesarArchivo(lines[i],nLinea+1,name); //Esta hecho para la primera fase del codigo solo lee los que tienen lex
 	      nLinea += 1;
 	    }
 	} catch (err) {
@@ -57,10 +72,22 @@ function leerArchivo(name,readline){
 	REPLaux(readline);
 }
 
+function imprimirErrores(readline){
+	console.log(errores);
+	REPLaux(readline);
+}
+
+function eliminarErrores(readline){
+	errores = [];
+	REPLaux(readline);
+}
+
 function REPLaux(readline){
 	readline.question('<Stokhos> ', entrada => {
 		var re = /.lex/gi;
 		var re1 = /.load/gi;
+		var re2 = /.failed/gi;
+		var re3 = /.reset/gi;
 		if (entrada == "."){
 			console.log("&")
 			readline.input.destroy();
@@ -74,8 +101,14 @@ function REPLaux(readline){
 			var fileName = name.replace(" ","");
 			leerArchivo(fileName,readline);
 		}
+		if (entrada.search(re2) != -1){
+			imprimirErrores(readline);
+		}
+		if (entrada.search(re3) != -1){
+			eliminarErrores(readline);
+		}
 		else{
-			if (entrada != "."){
+			if (entrada != "." && entrada.search(re) == -1 && entrada.search(re1) == -1 && entrada.search(re2) == -1 && entrada.search(re3) == -1){
 				console.log("ERROR: Interpretacion no implementada");
 			  REPLaux(readline);
 			}
@@ -102,14 +135,16 @@ function showTokens(array : Array<string>) : Array<string>{
 function guardarTokens(entrada) : Array<string> {
 	var parseo = parse(entrada);
 	var arreglo : Array<string> = [];
-	recursivaGuardar(parseo?.ast,arreglo);
+	if (parseo.ast != undefined || parseo.ast != null){
+		recursivaGuardar(parseo.ast.start,arreglo);
+	}
 	return arreglo;
 }
 
 function recursivaGuardar(siguiente, arreglo : Array<string>) :  void {
     while(siguiente != undefined){
-    	   arreglo.push(siguiente?.kind);
-    	   arreglo.push(siguiente?.value);
-    	   siguiente = siguiente?.next[0];
+    	   arreglo.push(siguiente.kind);
+    	   arreglo.push(siguiente.value);
+    	   siguiente = siguiente.next[0];
     }
 }
