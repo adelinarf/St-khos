85a0e837b9757cb45789474f3976a66d9bb5792b
Adelina Figueira <51302331+adelinarf@users.noreply.github.com>
2022-04-11 00:47:35 -0400

    se añaden las nuevas funciones

--- compilerWindows.bat
+++ compilerWindows.bat
@@ -2,9 +2,7 @@ cmd /c tspeg gramatica.peg parser.ts
 cmd /c tsc -t ES2021 -m commonjs parser.ts
 cmd /c tspeg parserGrammar.peg parser2.ts
 cmd /c tsc -t ES2021 -m commonjs parser2.ts
-cmd /c tsc Tree.ts
-cmd /c tsc parsingFunctions.ts
-cmd /c tsc validateLibrary.ts
+cmd /c tsc getString.ts
 cmd /c tsc VM.ts
 cmd /c tsc REPL.ts
 cmd /c tsc main.ts
